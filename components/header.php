a0147ff96ef34efdf967a480e4a18d7ec22fca0c
ArisGoi <aris.goi97@gmail.com>
2021-10-21 16:29:16 +0200

    add logo

--- components/header.php
+++ components/header.php
@@ -1,5 +1,5 @@
 <header>
     <div class="container-l">
-        <img class="logo" src="..." alt="">
+        <img class="logo" src="./components/spotify-logo.c24917d4.png" alt="">
     </div>
 </header>
