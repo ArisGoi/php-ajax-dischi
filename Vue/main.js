c0c002b88a885318882acfdd0ece7819bb3e2bd1
ArisGoi <aris.goi97@gmail.com>
2021-10-24 15:29:43 +0200

    add filter

--- Vue/main.js
+++ Vue/main.js
@@ -1,16 +1,26 @@
 const app = new Vue({
     el: "#root",
     data:{
-        albums: null
+        albums: null,
+        selGen: "all",
+        genList: [],
+    },
+    methods: {
     },
     created() {
         axios.get('./toJS.php')
         .then((response) => {
             this.albums = response.data;
-            console.log(response.data)
         })
         .catch(function (error){
             console.log(error);
         })
+    },
+    updated() {
+        this.albums.forEach(element => {
+            if(this.genList.includes(element.genre) == false){
+                this.genList.push(element.genre);
+            }
+        });
     }
 });
