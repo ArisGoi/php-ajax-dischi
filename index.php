0e53ec4fad4c6cd836bd4cbbd422099b1db19564
ArisGoi <aris.goi97@gmail.com>
2021-10-24 12:11:04 +0200

    add directories

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include __DIR__ . '/components/database.php'?>
+<?php include __DIR__ . '/database.php'?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vue Dischi</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style/style.css">
 </head>
 <body>
     <!-- Header -->
